de86838ae883db7a585b9f19e00a022010b5b19a
form5nv50Jose <152823657+form5nv50Jose@users.noreply.github.com>
2023-12-18 14:31:54 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,11 +8,10 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyDzZxk913MUi3ESjLwNK14W0t1c6HgUOWE",
-    authDomain: "bdfalumnox.firebaseapp.com",
-    projectId: "bdfalumnox",
-    storageBucket: "bdfalumnox.appspot.com",
-    messagingSenderId: "1002536188546",
-    appId: "1:1002536188546:web:cfa9fb33aec119e7fc529b",
-    measurementId: "G-5HHSLWQ24J"
+    apiKey: "AIzaSyD3q0sBm7FbV6ZvESMwcOaK11CfDzAQHBY",
+  authDomain: "bdfalumnojada.firebaseapp.com",
+  projectId: "bdfalumnojada",
+  storageBucket: "bdfalumnojada.appspot.com",
+  messagingSenderId: "695651312631",
+  appId: "1:695651312631:web:c39dbd1ee599b4ebb2c4f2"
 });
